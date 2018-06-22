61baf5cd51124825c8b48845daaff17c70ff6ef8
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-06-22 23:22:50 +0400

    Finished problem 2.1

---
+++ chapter_02/problem2_1.py
@@ -0,0 +1,14 @@
+#  Q1. We’ve seen that n = 42 is legal. What about 42 = n?
+#  A1. It produces a SyntaxError.
+
+# Q2. How about x = y = 1?
+# A2. It assigns 1 to y and then assigns y's value to x. So x and y both hold the value of 1.
+
+# Q3. n some languages every statement ends with a semi-colon, ;. What happens if you put a semi-colon at the end of a Python statement?
+# A3. It does not produce error, but sometimes linters complain about the semi-colon.
+
+# Q4. What if you put a period at the end of a statement?
+# A4. It produces a SyntaxError.
+
+# Q5. n math notation you can multiply x and y like this:  xy.  What happens if you try that in Python?
+# A5. It produces a SyntaxError.
