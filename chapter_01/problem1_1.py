b1aef062d1160b73306f0e71e9e79dd66aea0391
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-06-22 22:53:57 +0400

    Finished problem 1.1

---
+++ chapter_01/problem1_1.py
@@ -0,0 +1,14 @@
+# Q1. In a print statement, what happens if you leave out one of the parentheses, or both?
+# A1. You get SyntaxError
+
+# Q2. If you are trying to print a string, what happens if you leave out one of the quotation marks, or both?
+# A2. You get SyntaxError
+
+# Q3. You can use a minus sign to make a negative number like -2. What happens if you put a plus sign before a number? What about 2++2?
+# A3. Plus sign "makes" a number positive. 2++2 is equal to 4.
+
+# Q4. In math notation, leading zeros are ok, as in 02. What happens if you try this in Python?
+# A4. When you do this is Python, you get SyntaxError.
+
+# Q5. What happens if you have two values with no operator between them?
+# A5. You get SyntaxError.
