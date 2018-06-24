1673221ca63cb90390132b8000b033173f5b1479
Guga Gongadze <ggong13@freeuni.edu.ge>
2018-06-24 22:25:19 +0400

    Finished problem 6.3

---
+++ chapter_06/problem6_3.py
@@ -0,0 +1,17 @@
+def first(word):
+    return word[0]
+
+def last(word):
+    return word[-1]
+
+def middle(word):
+    return word[1:-1]
+
+def is_palindrome(word):
+    if len(word) <= 1:
+        return True
+
+    if first(word) != last(word):
+        return False
+
+    return is_palindrome(middle(word))
